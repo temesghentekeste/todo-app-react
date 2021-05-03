e4d7ed14c6b78fa66ea4048ac0299b253afba5ce
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-05-04 01:19:05 +0300

    Add todos state and render todos as list items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 function App() {
+  const [todos, setTodos] = useState([
+    'Take dogs for a walk',
+    'Take the rubbish out',
+  ]);
   return (
     <div className="App">
-      <h1>Greeting from Temie!</h1>
-      <input/>
+      <h1>Greeting from Temie! 📣 🎈 👍 ✌ </h1>
+      <input />
       <button>Add todo</button>
       <ul>
-        <li>Take dogs for a walk</li>
-        <li>Take the rubbish out</li>
+        {todos.map((todo) => (
+          <li> ✔️ {todo}</li>
+        ))}
       </ul>
     </div>
   );
