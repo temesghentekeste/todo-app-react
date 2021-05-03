d81ac467b5567f46ba8f0a21c5d7fc7592e08c56
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-05-04 00:50:48 +0300

    Add basic list items

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
       <h1>Greeting from Temie!</h1>
       <input/>
       <button>Add todo</button>
+      <ul>
+        <li>Take dogs for a walk</li>
+        <li>Take the rubbish out</li>
+      </ul>
     </div>
   );
 }
